74a19af95f661dc69ae2b3d26d2820ed97bfe015
annia valdes <millerl.mb79@gmail.com>
2020-08-13 13:11:01 -0500

    create the rote get find all tags with they associated product

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,8 +4,23 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
- 
-
+  // find all tags
+  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id','product_name','price','category_id'],
+        through: ProductTag,
+        as: 'product_tags'
+      }
+  ],
+  })
+    .then((getCategories) => res.json(getCategories))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
