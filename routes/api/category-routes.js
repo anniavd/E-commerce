d16e190b80895e1cffb1298195c9169b66872cfb
annia valdes <millerl.mb79@gmail.com>
2020-08-15 11:26:30 -0500

    update delete routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -79,23 +79,30 @@ router.put('/:id', (req, res) => {
 });
 
  // delete a category by its `id` value
-router.delete('/:id', (req, res) => {
- 
-  Category.destroy({
+ router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Product.destroy({
     where: {
-      id: req.params.id
+      category_id: req.params.id
     }
-  }).then(deleteCategories => {
-    if (!deleteCategories) {
-      res.status(404).json({ message: 'No categories found with this id' });
-      return;
-    }
-    res.json(deleteCategories);
-  })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+  }).then(() => {
+    Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryData => {
+      if (!categoryData) {
+          res.status(404).json({ message: 'No category found with this id'});
+          return;
+      }
+        res.json(categoryData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
     });
+  })
 });
 
 module.exports = router;
